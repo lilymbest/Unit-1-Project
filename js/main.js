79380d45679837ec068c0fe8d2cd1478b449f21d
Lily Best <lily.m.best@gmail.com>
2019-08-04 14:49:14 -0500

    added card array by planet

--- js/main.js
+++ js/main.js
@@ -1,5 +1,88 @@
 /*----- constants -----*/ 
 let board, score;
+let highScores = [];
+let cards = [
+    {
+        planet: moon,
+        backgroundColor: blue
+    },
+    {
+        planet: moon,
+        backgroundColor: red
+    },
+    {
+        planet: mercury,
+        backgroundColor: green
+    },
+    {
+        planet: mercury,
+        backgroundColor: blue
+    },
+    {
+        planet: venus,
+        backgroundColor: pink
+    },
+    {
+        planet: venus,
+        backgroundColor: yellow
+    },
+    {
+        plant: earth,
+        backgroundColor: purple
+    },
+    {
+        plant: earth,
+        backgroundColor: grey
+    },
+    {
+        planet: mars,
+        backgroundColor: grey
+    },
+    {
+        planet: mars,
+        backgroundColor: orange
+    },
+    {
+        planet: jupiter,
+        backgroundColor: green
+    },
+    {
+        planet: jupiter,
+        backgroundColor: yellow
+    },
+    {
+        planet: saturn,
+        backgroundColor: white
+    },
+    {
+        planet: saturn,
+        backgroundColor: purple
+    },
+    {
+        planet: uranus,
+        backgroundColor: pink
+    },
+    {
+        planet: uranus,
+        backgroundColor: red
+    },
+    {
+        planet: neptune,
+        backgroundColor: orange
+    },
+    {
+        planet: neptune,
+        backgroundColor: black
+    },
+    {
+        planet: pluto,
+        backgroundColor: white
+    },
+    {
+        planet: pluto,
+        backgroundColor: black
+    }
+]
 /*----- app's state (variables) -----*/ 
 
 /*----- cached element references -----*/ 
@@ -7,7 +90,7 @@ let board, score;
 /*----- event listeners -----*/ 
 
 document.querySelector('button').addEventListener('click', function (){
-    replay;
+    replay();
 });
 
 /*----- functions -----*/ 
@@ -18,6 +101,6 @@ function init(){
 }
 
 function replay(){
-    
+
 }
 
